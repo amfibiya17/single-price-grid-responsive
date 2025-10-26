b4c23f415e252486aa3f7beff728167fd5e7fd79
LONDON\yyates <yyates@london.edu>
2025-10-26 16:35:11 +0000

    Initialise the project

---
+++ components/CardContainer.tsx
@@ -0,0 +1,13 @@
+import Box from "@mui/material/Box";
+import React from "react";
+import CardHeader from "./CardHeader";
+
+const CardContainer = () => {
+  return (
+    <Box>
+      <CardHeader />
+    </Box>
+  );
+};
+
+export default CardContainer;
---
+++ components/CardHeader.tsx
@@ -0,0 +1,8 @@
+import Box from "@mui/material/Box";
+import React from "react";
+
+const CardHeader = () => {
+  return <Box>CardHeader</Box>;
+};
+
+export default CardHeader;
